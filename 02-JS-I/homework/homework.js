7687b6d151b726e6469d7b8c5ee4eba45ec8fd59
CARLOS <carlosmaraz@hotmail.com>
2022-06-02 01:16:24 -0300

    ejercicio 12

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,14 +60,22 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (x == y){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (str1.length === str2.length){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function menosQueNoventa(num) {
