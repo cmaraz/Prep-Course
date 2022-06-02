9037c3a294d62ffdcc2360915ccaabf0ccef53e5
CARLOS <carlosmaraz@hotmail.com>
2022-06-02 01:41:09 -0300

    ejercicio 17

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -110,7 +110,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num / 2 == 0){
+  if (num % 2 == 0){
     return true;
   } else {
     return false;
@@ -121,7 +121,11 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 1 ){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function elevarAlCuadrado(num) {
