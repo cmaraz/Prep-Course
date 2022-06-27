85548d7e7d3e840cd4b638ff2bf053b4b9335668
CARLOS <carlosmaraz@hotmail.com>
2022-06-26 23:00:47 -0300

    pasando 2

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto = {
+    nombre,
+    edad,
+    meow : function (){
+    return "Meow!"
+    }
+    
+  }
+  return  objeto
 }
 
 function agregarPropiedad (objeto, property) {
